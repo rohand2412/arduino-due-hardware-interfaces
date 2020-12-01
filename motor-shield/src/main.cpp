fbbf7446b5379e96fe5cc145e4762436a9d7dc82
Advait Duggi <one.giant.cowboy@gmail.com>
2020-11-30 22:40:46 -0500

    Interfaced the Adafruit Motor-Shield, allows for input of 2 motor speeds

--- motor-shield/src/main.cpp
+++ motor-shield/src/main.cpp
@@ -1,9 +1,89 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *Motorone = AFMS.getMotor(1);
+Adafruit_DCMotor *Motortwo = AFMS.getMotor(1);
 
-void setup() {
-  // put your setup code here, to run once:
+
+void setup() 
+{
+  AFMS.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void runmotor(int speedone, int speedtwo)
+{
+  bool negone;
+  bool negtwo;
+  if(speedone <= -255 || speedone >= 255)
+  {
+    if(speedone <= -255)
+    {
+      speedone = -255;
+    }
+    else
+    {
+      speedone = 255;
+    }
+  }
+  if(speedtwo <= -255 || speedtwo >= 255)
+  {
+    if(speedtwo <= -255)
+    {
+      speedtwo = -255;
+    }
+    else
+    {
+      speedtwo = 255;
+    }
+  }
+  if(speedone<= 0)
+  {
+    if(speedone == 0)
+    {
+      Motorone->setSpeed(0);
+      Motorone->run(RELEASE);
+    }
+    else
+    {
+      negone = true;
+    }
+  }
+  if(speedtwo <= 0)
+  {
+    if(speedone == 0)
+    {
+      Motortwo->setSpeed(0);
+      Motortwo->run(RELEASE);
+    }
+    else
+    {
+      negtwo = true;
+    }
+  }
+  if(negone)
+  {
+    Motortwo->setSpeed(speedone);
+    Motortwo->run(BACKWARD);
+  }
+  else if(!negone)
+  {
+    Motortwo->setSpeed(speedone);
+    Motortwo->run(FORWARD);
+  }
+  if(negtwo)
+  {
+    Motortwo->setSpeed(speedtwo);
+    Motortwo->run(BACKWARD);
+  }
+  else if(!negtwo)
+  {
+    Motortwo->setSpeed(speedtwo);
+    Motortwo->run(FORWARD);
+  }
+}
+void loop() 
+{
+  runmotor(-20, 20);
 }
