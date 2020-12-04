99e55a2fa76d08941d7535ecfa896195b6787d62
Advait Duggi <one.giant.cowboy@gmail.com>
2020-12-03 23:46:37 -0500

    This includes a Servo interface where it sweeps the servo 180 degrees to the right then 180 degrees to the left

--- servo/src/main.cpp
+++ servo/src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+Servo myservo;
+int pos = 0;
+void sweepservo()
+{
+  for (pos = 0; pos <= 180; pos += 1) 
+  { 
+    myservo.write(pos);             
+    delay(15);                     
+  }
+  for (pos = 180; pos >= 0; pos -= 1) 
+  { 
+    myservo.write(pos);              
+    delay(15);                       
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup() 
+{
+  myservo.attach(9);  
+}
+
+void loop() 
+{
+  sweepservo();
 }
