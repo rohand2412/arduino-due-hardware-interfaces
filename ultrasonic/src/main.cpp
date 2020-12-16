3715f7a774d3f49eb7d5e10ba9b53119968d4b5e
Rohan Dugad <rohand2468@gmail.com>
2020-12-15 22:32:06 -0500

    Switched to usage of Ultrasonic_Wrapper library

--- ultrasonic/src/main.cpp
+++ ultrasonic/src/main.cpp
@@ -1,19 +1,17 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <Ultrasonic.h>
+#include <Ultrasonic_Wrapper.h>
 
-Ultrasonic ultrasonicLeft(22, 24, 20);
-Ultrasonic ultrasonicRight(22, 28, 20);
+unsigned int echoPins[4] = {53, 51, 49, 47};
+Ultrasonic_Wrapper ultrasonics(52, echoPins, 20, 4);
 
-void ultrasonicLeftEchoPinISR()
-{
-  ultrasonicLeft.echoPinISR();
-}
+void ultrasonicFrontEchoPinISR() { ultrasonics.echoPinISR(0); }
 
-void ultrasonicRightEchoPinISR()
-{
-  ultrasonicRight.echoPinISR();
-}
+void ultrasonicLeftEchoPinISR() { ultrasonics.echoPinISR(1); }
+
+void ultrasonicRightEchoPinISR() { ultrasonics.echoPinISR(2); }
+
+void ultrasonicBackEchoPinISR() { ultrasonics.echoPinISR(3); }
 
 void setup() 
 {
@@ -21,14 +19,22 @@ void setup()
   while (!Serial);
   delay(300);
 
-  ultrasonicLeft.begin(ultrasonicLeftEchoPinISR);
-  ultrasonicRight.begin(ultrasonicRightEchoPinISR);
+  void (*ultrasonicISR[4])() = {ultrasonicFrontEchoPinISR,
+                                ultrasonicLeftEchoPinISR, 
+                                ultrasonicRightEchoPinISR, 
+                                ultrasonicBackEchoPinISR};
+  ultrasonics.begin(ultrasonicISR);
 }
 
 void loop() {
-  ultrasonicLeft.update();
-  ultrasonicRight.update();
-  Serial.print(ultrasonicLeft.getDistance());
-  Serial.print("\t");
-  Serial.println(ultrasonicRight.getDistance());
+  ultrasonics.update();
+  Serial.print("Front: ");
+  Serial.print(ultrasonics.getDistance(0));
+  Serial.print("\tLeft: ");
+  Serial.print(ultrasonics.getDistance(1));
+  Serial.print("\tRight: ");
+  Serial.print(ultrasonics.getDistance(2));
+  Serial.print("\tBack: ");
+  Serial.print(ultrasonics.getDistance(3));
+  Serial.print("\n");
 }
