ccaa5045dc4ff9bcce04de0d1ef637973973f510
Rohan Dugad <rohand2468@gmail.com>
2020-12-17 01:00:39 -0500

    Removed unnecessary whitespace

--- servo/src/main.cpp
+++ servo/src/main.cpp
@@ -19,15 +19,15 @@ Servo_Wrapper myservo(45, 135);
 int pos = 0;
 void sweepservo()
 {
-  for (pos = 0; pos <= 180; pos += 1) 
-  { 
-    myservo.write(pos);             
-    delay(15);                     
+  for (pos = 0; pos <= 180; pos += 1)
+  {
+    myservo.write(pos);
+    delay(15);
   }
-  for (pos = 180; pos >= 0; pos -= 1) 
-  { 
-    myservo.write(pos);              
-    delay(15);                       
+  for (pos = 180; pos >= 0; pos -= 1)
+  {
+    myservo.write(pos);
+    delay(15);
   }
 }
 
