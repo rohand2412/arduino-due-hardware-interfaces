986af26042d15f8efca30a78296727dea0ab4b64
Rohan Dugad <rohand2468@gmail.com>
2020-12-19 03:08:20 -0500

    Implemented multiple instances of Encoder_Wrapper class

--- encoder/src/main.cpp
+++ encoder/src/main.cpp
@@ -2,43 +2,34 @@
 #include <Wire.h>
 #include <Encoder_Wrapper.h>
 
-// Change these pin numbers to the pins connected to your encoder.
-//   Best Performance: both pins have interrupt capability
-//   Good Performance: only the first pin has interrupt capability
-//   Low Performance:  neither pin has interrupt capability
-unsigned int pins[4] = {46, 44, 50, 48};
-Encoder_Wrapper encoders(pins, 2);
-//   avoid using pins with LEDs attached
+unsigned int twoPins[2] = {50, 48};
+Encoder_Wrapper encoder(twoPins, 1);
+unsigned int fourPins[4] = {46, 44, 50, 48};
+Encoder_Wrapper encoders(fourPins, 2);
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
   while (!Serial)
     ;
   delay(300);
-  Serial.println("TwoKnobs Encoder Test:");
 }
 
-long positionLeft  = -999;
-long positionRight = -999;
-
-void loop() {
-  long newLeft, newRight;
-  newLeft = encoders.getCount(0);
-  newRight = encoders.getCount(1);
-  if (newLeft != positionLeft || newRight != positionRight) {
-    Serial.print("Left = ");
-    Serial.print(newLeft);
-    Serial.print(", Right = ");
-    Serial.print(newRight);
-    Serial.println();
-    positionLeft = newLeft;
-    positionRight = newRight;
-  }
-  // if a character is sent from the serial monitor,
-  // reset both back to zero.
-  if (Serial.available()) {
-    Serial.read();
-    Serial.println("Reset both knobs to zero");
-    encoders.resetCount();
-  }
+void loop() 
+{
+  Serial.println(encoders.getCount(0));
+  Serial.println(encoders.getCount(1));
+  encoders.setCount(100, 0);
+  encoders.setCount(200, 1);
+  Serial.println(encoders.getCount(0));
+  Serial.println(encoders.getCount(1));
+  encoders.resetCount();
+  Serial.println(encoders.getCount(0));
+  Serial.println(encoders.getCount(1));
+  Serial.println(encoders.getPin(0, 0));
+  Serial.println(encoders.getPin(0, 1));
+  Serial.println(encoders.getPin(1, 0));
+  Serial.println(encoders.getPin(1, 1));
+  while (true)
+    ;
 }
