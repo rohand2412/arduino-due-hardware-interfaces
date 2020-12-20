217b6f26a0775128f68b1a4ad9b2babb65eeb122
Rohan Dugad <rohand2468@gmail.com>
2020-12-20 03:51:26 -0500

    Implemented ghost Encoder

--- encoder/src/main.cpp
+++ encoder/src/main.cpp
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <Encoder_Wrapper.h>
 
-unsigned int twoPins[2] = {50, 48};
+unsigned int twoPins[2] = {25, 23};
 Encoder_Wrapper encoder(twoPins, 1);
 unsigned int fourPins[4] = {46, 44, 50, 48};
 Encoder_Wrapper encoders(fourPins, 2);
@@ -17,13 +17,20 @@ void setup()
 
 void loop() 
 {
+  Serial.println(encoder.getCount());
   Serial.println(encoders.getCount(0));
   Serial.println(encoders.getCount(1));
+  encoder.setCount(-100, 0);
   encoders.setCount(100, 0);
   encoders.setCount(200, 1);
+  Serial.println(encoder.getCount());
   Serial.println(encoders.getCount(0));
   Serial.println(encoders.getCount(1));
+  encoder.resetCount();
   encoders.resetCount();
+  Serial.println(encoder.getCount());
+  Serial.println(encoder.getPin(0, 0));
+  Serial.println(encoder.getPin(0, 1));
   Serial.println(encoders.getCount(0));
   Serial.println(encoders.getCount(1));
   Serial.println(encoders.getPin(0, 0));
