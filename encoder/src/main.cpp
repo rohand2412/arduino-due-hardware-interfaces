534a27984bc51abc837e018de2162c0e9eff6172
Rohan Dugad <rohand2468@gmail.com>
2020-12-21 21:58:08 -0500

    Restructured into object array

--- encoder/src/main.cpp
+++ encoder/src/main.cpp
@@ -9,61 +9,36 @@ void setup()
     ;
   delay(300);
 
-  size_t sensorNum = 3;
-  unsigned int newEncoderPins[sensorNum * Encoder_Wrapper::PINS_PER_SENSOR] = {25, 23, 29, 27, 31, 33};
-  Encoder_Wrapper newEncoders(newEncoderPins, sensorNum);
-  unsigned int mixedEncoderPins[sensorNum * Encoder_Wrapper::PINS_PER_SENSOR] = {31, 33, 46, 44, 50, 48};
-  Encoder_Wrapper mixedEncoders(mixedEncoderPins, sensorNum);
-  unsigned int repeatedEncoderPins[sensorNum * Encoder_Wrapper::PINS_PER_SENSOR] = {29, 27, 25, 23, 46, 44};
-  Encoder_Wrapper repeatedEncoders(repeatedEncoderPins, sensorNum);
+  size_t encoderNum = 3;
+  size_t wrapperNum = 3;
+  Encoder_Wrapper encoders[wrapperNum];
+  unsigned int newEncoderPins[encoderNum * Encoder_Wrapper::PINS_PER_SENSOR] = {25, 23, 29, 27, 31, 33};
+  encoders[0].begin(newEncoderPins, encoderNum);
+  unsigned int mixedEncoderPins[encoderNum * Encoder_Wrapper::PINS_PER_SENSOR] = {31, 33, 46, 44, 50, 48};
+  encoders[1].begin(mixedEncoderPins, encoderNum);
+  unsigned int repeatedEncoderPins[encoderNum * Encoder_Wrapper::PINS_PER_SENSOR] = {29, 27, 25, 23, 46, 44};
+  encoders[2].begin(repeatedEncoderPins, encoderNum);
 
   Serial.print("Total Number of Initialized Encoders: ");
   Serial.println(Encoder_Wrapper::getTotalSensorNum());
 
-  for (size_t sensor = 0; sensor < newEncoders.getSensorNum(); sensor++)
+  for (size_t wrapper = 0; wrapper < wrapperNum; wrapper++)
   {
-    Serial.print("newEncoders getCount: ");
-    Serial.println(newEncoders.getCount(sensor));
-    newEncoders.setCount(sensor, -100);
-    Serial.print("newEncoders getCount: ");
-    Serial.println(newEncoders.getCount(sensor));
-    newEncoders.resetCount();
-    Serial.print("newEncoders getCount: ");
-    Serial.println(newEncoders.getCount(sensor));
-    Serial.print("newEncoders getPin: ");
-    Serial.print(newEncoders.getPin(sensor, Encoder_Wrapper::ENCODER_OUT_A));
-    Serial.print(" ");
-    Serial.println(newEncoders.getPin(sensor, Encoder_Wrapper::ENCODER_OUT_B));
-  }
-  for (size_t sensor = 0; sensor < mixedEncoders.getSensorNum(); sensor++)
-  {
-    Serial.print("mixedEncoders getCount: ");
-    Serial.println(mixedEncoders.getCount(sensor));
-    mixedEncoders.setCount(sensor, 100);
-    Serial.print("mixedEncoders getCount: ");
-    Serial.println(mixedEncoders.getCount(sensor));
-    mixedEncoders.resetCount();
-    Serial.print("mixedEncoders getCount: ");
-    Serial.println(mixedEncoders.getCount(sensor));
-    Serial.print("mixedEncoders getPin: ");
-    Serial.print(mixedEncoders.getPin(sensor, Encoder_Wrapper::ENCODER_OUT_A));
-    Serial.print(" ");
-    Serial.println(mixedEncoders.getPin(sensor, Encoder_Wrapper::ENCODER_OUT_B));
-  }
-  for (size_t sensor = 0; sensor < repeatedEncoders.getSensorNum(); sensor++)
-  {
-    Serial.print("repeatedEncoders getCount: ");
-    Serial.println(repeatedEncoders.getCount(sensor));
-    repeatedEncoders.setCount(sensor, -100);
-    Serial.print("repeatedEncoders getCount: ");
-    Serial.println(repeatedEncoders.getCount(sensor));
-    repeatedEncoders.resetCount();
-    Serial.print("repeatedEncoders getCount: ");
-    Serial.println(repeatedEncoders.getCount(sensor));
-    Serial.print("repeatedEncoders getPin: ");
-    Serial.print(repeatedEncoders.getPin(sensor, Encoder_Wrapper::ENCODER_OUT_A));
-    Serial.print(" ");
-    Serial.println(repeatedEncoders.getPin(sensor, Encoder_Wrapper::ENCODER_OUT_B));
+    for (size_t sensor = 0; sensor < encoders[wrapper].getSensorNum(); sensor++)
+    {
+      Serial.print("encoders getCount: ");
+      Serial.println(encoders[wrapper].getCount(sensor));
+      encoders[wrapper].setCount(sensor, -100);
+      Serial.print("encoders getCount: ");
+      Serial.println(encoders[wrapper].getCount(sensor));
+      encoders[wrapper].resetCount();
+      Serial.print("encoders getCount: ");
+      Serial.println(encoders[wrapper].getCount(sensor));
+      Serial.print("encoders getPin: ");
+      Serial.print(encoders[wrapper].getPin(sensor, Encoder_Wrapper::ENCODER_OUT_A));
+      Serial.print(" ");
+      Serial.println(encoders[wrapper].getPin(sensor, Encoder_Wrapper::ENCODER_OUT_B));
+    }
   }
 }
 
