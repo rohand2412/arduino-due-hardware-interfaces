caf195ca1340216ecbc06047d66c67a3004b1751
Rohan Dugad <rohand2468@gmail.com>
2021-01-22 18:34:22 -0500

    Fixed indentation

--- imu/src/main.cpp
+++ imu/src/main.cpp
@@ -18,36 +18,36 @@ adafruit_bno055_offsets_t offsets{13, -48, -25,
 /**************************************************************************/
 void setup(void)
 {
-  Serial.begin(115200);
-  while (!Serial)
-    ;
-  delay(300);
+    Serial.begin(115200);
+    while (!Serial)
+        ;
+    delay(300);
 
-  const size_t wrapperNum = 3;
-  IMU_Wrapper imus[wrapperNum];
-  imus[0].createSensor(43);
-  imus[0].setOffsets(offsets);
-  imus[0].begin();
-  imus[1].createSensor(43);
-  imus[1].setOffsets(offsets);
-  imus[1].begin();
-  imus[2].createSensor(43);
-  imus[2].setOffsets(offsets);
-  imus[2].begin();
+    const size_t wrapperNum = 3;
+    IMU_Wrapper imus[wrapperNum];
+    imus[0].createSensor(43);
+    imus[0].setOffsets(offsets);
+    imus[0].begin();
+    imus[1].createSensor(43);
+    imus[1].setOffsets(offsets);
+    imus[1].begin();
+    imus[2].createSensor(43);
+    imus[2].setOffsets(offsets);
+    imus[2].begin();
 
-  Serial.print("Total Number of Initialized Imus: ");
-  Serial.println(IMU_Wrapper::getTotalSensorNum());
+    Serial.print("Total Number of Initialized Imus: ");
+    Serial.println(IMU_Wrapper::getTotalSensorNum());
 
-  for (size_t wrapper = 0; wrapper < wrapperNum; wrapper++)
-  {
-    imus[wrapper].displayOffsets();
-    imus[wrapper].displaySensorDetails();
-    imus[wrapper].displaySensorStatus();
-    imus[wrapper].displayCalStatus();
-    imus[wrapper].displayOrientation();
-  }
+    for (size_t wrapper = 0; wrapper < wrapperNum; wrapper++)
+    {
+        imus[wrapper].displayOffsets();
+        imus[wrapper].displaySensorDetails();
+        imus[wrapper].displaySensorStatus();
+        imus[wrapper].displayCalStatus();
+        imus[wrapper].displayOrientation();
+    }
 
-  delay(2000);
+    delay(2000);
 }
 
 /**************************************************************************/
@@ -58,36 +58,36 @@ void setup(void)
 /**************************************************************************/
 void loop(void)
 {
-  if (once)
-  {
-    localImu.setOffsets(offsets);
-    totalImu.setOffsets(offsets);
-    localImu.begin();
-    totalImu.begin();
-  }
+    if (once)
+    {
+        localImu.setOffsets(offsets);
+        totalImu.setOffsets(offsets);
+        localImu.begin();
+        totalImu.begin();
+    }
 
-  /* Get a new sensor information */
-  localImu.update();
-  totalImu.update();
+    /* Get a new sensor information */
+    localImu.update();
+    totalImu.update();
 
-  Serial.print(localImu.getYaw());
-  Serial.print("\t");
-  Serial.print(localImu.getPitch());
-  Serial.print("\t");
-  Serial.print(localImu.getRoll());
-  Serial.print("\t\t");
-  Serial.print(totalImu.getYaw());
-  Serial.print("\t");
-  Serial.print(totalImu.getPitch());
-  Serial.print("\t");
-  Serial.print(totalImu.getRoll());
-  Serial.print("\n");
+    Serial.print(localImu.getYaw());
+    Serial.print("\t");
+    Serial.print(localImu.getPitch());
+    Serial.print("\t");
+    Serial.print(localImu.getRoll());
+    Serial.print("\t\t");
+    Serial.print(totalImu.getYaw());
+    Serial.print("\t");
+    Serial.print(totalImu.getPitch());
+    Serial.print("\t");
+    Serial.print(totalImu.getRoll());
+    Serial.print("\n");
 
-  if (Serial.available())
-  {
-    Serial.read();
-    Serial.println("Reset localImu");
-    localImu.reset();
-    localImu.resetSensor();
-  }
+    if (Serial.available())
+    {
+        Serial.read();
+        Serial.println("Reset localImu");
+        localImu.reset();
+        localImu.resetSensor();
+    }
 }
