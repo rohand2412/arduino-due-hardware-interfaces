5eb7687a7167bc5e009a800355c83fc7b6fd5e58
Rohan Dugad <rohand2468@gmail.com>
2021-01-22 18:33:34 -0500

    Converted to program of test cases

--- imu/src/main.cpp
+++ imu/src/main.cpp
@@ -2,7 +2,14 @@
 #include <Wire.h>
 #include <IMU_Wrapper.h>
 
-IMU_Wrapper myImu(43);
+bool once = true;
+IMU_Wrapper localImu(43);
+IMU_Wrapper totalImu(43);
+
+adafruit_bno055_offsets_t offsets{13, -48, -25,
+                                  -101, 127, 438,
+                                  -2, -2, 0,
+                                  1000, 835};
 
 /**************************************************************************/
 /*
@@ -12,21 +19,35 @@ IMU_Wrapper myImu(43);
 void setup(void)
 {
   Serial.begin(115200);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
+  while (!Serial)
+    ;
+  delay(300);
 
-  adafruit_bno055_offsets_t offsets{13, -48, -25,
-                                    -101, 127, 438,
-                                    -2, -2, 0,
-                                    1000, 835};
-  myImu.setOffsets(offsets);
+  const size_t wrapperNum = 3;
+  IMU_Wrapper imus[wrapperNum];
+  imus[0].createSensor(43);
+  imus[0].setOffsets(offsets);
+  imus[0].begin();
+  imus[1].createSensor(43);
+  imus[1].setOffsets(offsets);
+  imus[1].begin();
+  imus[2].createSensor(43);
+  imus[2].setOffsets(offsets);
+  imus[2].begin();
 
-  myImu.begin();
+  Serial.print("Total Number of Initialized Imus: ");
+  Serial.println(IMU_Wrapper::getTotalSensorNum());
 
-  /* Display some basic information on this sensor */
-  myImu.displaySensorDetails();
+  for (size_t wrapper = 0; wrapper < wrapperNum; wrapper++)
+  {
+    imus[wrapper].displayOffsets();
+    imus[wrapper].displaySensorDetails();
+    imus[wrapper].displaySensorStatus();
+    imus[wrapper].displayCalStatus();
+    imus[wrapper].displayOrientation();
+  }
 
-  /* Optional: Display current status */
-  myImu.displaySensorStatus();
+  delay(2000);
 }
 
 /**************************************************************************/
@@ -37,15 +58,36 @@ void setup(void)
 /**************************************************************************/
 void loop(void)
 {
+  if (once)
+  {
+    localImu.setOffsets(offsets);
+    totalImu.setOffsets(offsets);
+    localImu.begin();
+    totalImu.begin();
+  }
+
   /* Get a new sensor information */
-  myImu.update();
+  localImu.update();
+  totalImu.update();
 
-  /* Display the floating point data */
-  myImu.displayOrientation();
+  Serial.print(localImu.getYaw());
+  Serial.print("\t");
+  Serial.print(localImu.getPitch());
+  Serial.print("\t");
+  Serial.print(localImu.getRoll());
+  Serial.print("\t\t");
+  Serial.print(totalImu.getYaw());
+  Serial.print("\t");
+  Serial.print(totalImu.getPitch());
+  Serial.print("\t");
+  Serial.print(totalImu.getRoll());
+  Serial.print("\n");
 
-  /* Optional: Display calibration status */
-  myImu.displayCalStatus();
-
-  /* Optional: Display sensor status (debug only) */
-  // myImu.displaySensorStatus();
+  if (Serial.available())
+  {
+    Serial.read();
+    Serial.println("Reset localImu");
+    localImu.reset();
+    localImu.resetSensor();
+  }
 }
