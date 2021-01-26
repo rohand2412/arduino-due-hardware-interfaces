88b7dd9ce9e49c06fd9c85a0f7e4f3644a76d17d
Rohan Dugad <rohand2468@gmail.com>
2021-01-26 00:33:31 -0500

    Implemented send and receive functionality

--- rpi-serial/src/main.cpp
+++ rpi-serial/src/main.cpp
@@ -2,15 +2,20 @@
 #include <Wire.h>
 #include <Serial_Wrapper.h>
 
+const size_t bufferLen = 10;
+uint8_t buffer[bufferLen];
+size_t dataLen = 0;
+
 void setup() 
 {
     Serial_Wrapper::begin(750000, Serial);
 
     Serial_Wrapper::begin(115200, Serial3);
+    Serial_Wrapper::setDefault(Serial3);
 }
 
 void loop() 
 {
-    Serial.println("It works!");
-    Serial3.write('a');
+    dataLen = Serial_Wrapper::receive(buffer, bufferLen);
+    Serial_Wrapper::send(buffer, dataLen);
 }
