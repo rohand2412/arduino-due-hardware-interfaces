4a200727398c8146d108e372a2b9be104f08905b
Rohan Dugad <rohand2468@gmail.com>
2021-02-25 00:49:49 -0500

    Implemented detection code

--- button/src/main.cpp
+++ button/src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const uint8_t buttonPin = 40;
+bool buttonState = false;
+bool dormantState = false;
+unsigned int updateCounter = 0;
+const unsigned long debounceDelay = 30;
+volatile unsigned long lastDebounce = millis() - debounceDelay;
+
+void buttonISR()
+{
+  lastDebounce = millis();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(buttonPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonISR, CHANGE);
+}
+
+void loop()
+{
+  bool curButtonState = digitalRead(buttonPin);
+
+  if ((millis() - lastDebounce) > debounceDelay)
+  {
+    if (curButtonState != buttonState)
+    {
+      buttonState = curButtonState;
+
+      if (buttonState == HIGH) {
+        dormantState = !dormantState;
+        updateCounter++;
+      }
+    }
+  }
+
+  Serial.print(dormantState);
+  Serial.print("\t");
+  Serial.print(updateCounter);
+  Serial.print("\t");
+  Serial.println(curButtonState);
 }
