927ece43eab01776f13c1c597a76fdbc61b64916
Rohan Dugad <rohand2468@gmail.com>
2021-03-23 01:50:20 -0400

    Used robot class to configure for line

--- set-line/src/main.cpp
+++ set-line/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <Robot.h>
 
-void setup() {
-  // put your setup code here, to run once:
+Robot robot;
+
+void ultrasonicFrontISR() { robot.getUltrasonics().echoPinISR(Ultrasonic_Wrapper::ULTRASONIC_FRONT); }
+
+void ultrasonicLeftISR() { robot.getUltrasonics().echoPinISR(Ultrasonic_Wrapper::ULTRASONIC_LEFT); }
+
+void ultrasonicRightISR() { robot.getUltrasonics().echoPinISR(Ultrasonic_Wrapper::ULTRASONIC_RIGHT); }
+
+void ultrasonicBackISR() { robot.getUltrasonics().echoPinISR(Ultrasonic_Wrapper::ULTRASONIC_BACK); }
+
+void setup()
+{
+    void (*ultrasonicISR[4])() = {ultrasonicFrontISR,
+                                  ultrasonicLeftISR,
+                                  ultrasonicRightISR,
+                                  ultrasonicBackISR};
+    robot.begin(ultrasonicISR);
+
+    robot.setLine();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{}
