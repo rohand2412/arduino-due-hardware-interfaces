cec4a4545022463746c27498427cea7eb9857424
Rohan Dugad <rohand2468@gmail.com>
2021-05-28 04:15:08 -0400

    Tested ball capturing with motor movement

--- servo/src/main.cpp
+++ servo/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Servo_Wrapper.h>
+#include <Motor_Wrapper.h>
 
 /*==========================
 
@@ -12,10 +13,15 @@ GND         -----  BLACK
 
 ==========================*/
 
-const unsigned int LOWER_BOUND = 45;
-const unsigned int UPPER_BOUND = 135;
+size_t motorNum = 2;
+unsigned int motorPorts[2] = {Motor_Wrapper::SHIELD_M1, Motor_Wrapper::SHIELD_M3};
+Motor_Wrapper motors(motorPorts, motorNum);
+
+const unsigned int LOWER_BOUND = 35;
+const unsigned int UPPER_BOUND = 140;
 const unsigned int BOUND_DIF = UPPER_BOUND - LOWER_BOUND;
 Servo_Wrapper myservo(LOWER_BOUND, UPPER_BOUND);
+Servo_Wrapper camServo(47, 137);
 
 unsigned int pos = 0;
 void sweepservo()
@@ -42,17 +48,82 @@ void setup()
     ;
   delay(300);
 
-  //510 and 2520 - min/max PWM signal range 
-  //myservo.attach(Servo_Wrapper::SERVO_S1, 510, 2520);
+  {
+    unsigned int encoderPins[motorNum * Encoder_Wrapper::PINS_PER_SENSOR] = {46, 44, 50, 48};
+    motors.setEncoders(encoderPins);
+  }
 
-  myservo.attach(Servo_Wrapper::SERVO_S2);
+  motors.setPid(0.8, 0.75, 0, 0.75, 0.7, 0, Motor_Wrapper::MOTOR_LEFT);
+  motors.setPid(0.85, 0.8, 0, 0.8, 0.75, 0, Motor_Wrapper::MOTOR_RIGHT);
+  motors.setSpeedMultiplier(Motor_Wrapper::MOTOR_FLIP, Motor_Wrapper::MOTOR_LEFT);
+  motors.setSpeedMultiplier(Motor_Wrapper::MOTOR_NO_FLIP, Motor_Wrapper::MOTOR_RIGHT);
+
+  motors.begin();
+
+  //510 and 2520 - min/max PWM signal range 
+  myservo.attach(Servo_Wrapper::SERVO_S1, 510, 2520);
+  myservo.write(85);
+  camServo.attach(Servo_Wrapper::SERVO_S2);
+  camServo.write(15);
+
+  //   myservo.attach(Servo_Wrapper::SERVO_S2);
+  delay(2000);
 }
+
+unsigned long DELAY_MS = 4000;
+unsigned long lastTimeMotor;
+bool startedRun = false;
+bool alreadyRun = false;
+int increment = -1;
+
+unsigned long INTERVAL_MS = 20;
+unsigned long lastTimeServo = millis() - INTERVAL_MS;
+unsigned int slam_counter = 0;
 
 void loop() 
 {
-  //sweepservo();
-  myservo.write(15);
-}
+    motors.update();
 
-//Line - 35 deg
-//Evac - 15 deg
+    if (millis() >= lastTimeServo + INTERVAL_MS)
+    {
+        myservo.write(myservo.read() + increment);
+        if (myservo.read() > 85)
+        {
+            if (!(slam_counter % 3))
+            {
+                INTERVAL_MS = 20;
+            }
+            else
+            {
+                INTERVAL_MS = 0;
+            }
+            slam_counter++;
+            increment = -1;
+            delay(500);
+        }
+        else if (myservo.read() <= 0)
+        {
+            INTERVAL_MS = 20;
+            increment = 1;
+            delay(500);
+        }
+        
+        lastTimeServo = millis();
+    }
+
+    // if (deg <= 30 && !startedRun && !alreadyRun)
+    // {
+    //     motors.run(-0.25);
+    //     lastTimeMotor = millis();
+    //     startedRun = true;
+    // }
+
+    // if ((millis() >= lastTimeMotor + DELAY_MS) && !alreadyRun && startedRun)
+    // {
+    //     motors.stop();
+    //     delay(2000);
+    //     myservo.write(85);
+    //     startedRun = false;
+    //     alreadyRun = true;
+    // }
+}
